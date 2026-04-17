eb5557372d82ab3cd2f4df9afd3191a95459c42f
Sebastián Gallegos F. <59512190+SebaGallegos@users.noreply.github.com>
2026-04-17 00:54:01 -0400

    fix: removed old code

--- src/audio_engine.rs
+++ src/audio_engine.rs
@@ -51,33 +51,3 @@ pub fn init_engine(song_path: String, command_receiver: Receiver<AudioCommand>) 
         }
     });
 }
-
-// pub fn play(file_path: &str) -> Result<(), String> {
-//     // find the default output device
-//     let mut stream_handle = DeviceSinkBuilder::open_default_sink()
-//         .map_err(|_| "Default audio hardware output device not found.")?;
-
-//     // to prevent the log message "Stream dropped while still playing" when the player is dropped
-//     stream_handle.log_on_drop(false);
-
-//     // create a new player
-//     let player = Player::connect_new(stream_handle.mixer());
-
-//     // open the audio file
-//     let file = File::open(file_path)
-//         .map_err(|_| format!("Cannot open audio file: '{}', please check the path.", file_path))?;
-
-//     // decode the audio file
-//     let reader = BufReader::new(file);
-//     let source = Decoder::new(reader)
-//         .map_err(|_| "The file not have a supported audio format or is corrupted.")?;
-
-//     // play the audio
-//     player.append(source);
-
-//     // keep the player alive until the audio finishes playing
-//     player.sleep_until_end();
-
-//     // return success
-//     Ok(())
-// }
